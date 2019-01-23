bf60c1b7abe377e3055975eaaf8c72df3d9ef465
BrittanyTinnin <bntinnin7@gmail.com>
2019-01-23 02:11:18 -0500

    Done.

--- index.js
+++ index.js
@@ -7,16 +7,50 @@ function getToken() {
 function forkRepo() {
   const repo = 'learn-co-curriculum/js-ajax-fetch-lab';
   //use fetch to fork it!
+  fetch(`https://api.github.com/repos/${repo}/forks`,
+  {
+    method: 'POST',
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+  })
+  .then(res => res.json())
+  .then(json => showResults(json))
 }
 
 function showResults(json) {
   //use this function to display the results from forking via the API
+  document.getElementById('results').innerHTML = `<a href=${json.html_url}>Show Fork</a>`
 }
 
 function createIssue() {
+  const repo = 'BrittanyTinnin/js-ajax-fetch-lab'
+  const title = document.getElementById("title").value
+  const body = document.getElementById("body").value
+
+  const postData = {
+    title: title,
+    body: body
+  }
   //use this function to create an issue based on the values input in index.html
+  fetch(`https://api.github.com/repos/${repo}/issues`,
+  {
+    method: 'POST',
+    body: JSON.stringify(postData),
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+  })
+  .then(res => res.json())
 }
 
 function getIssues() {
   //once an issue is submitted, fetch all open issues to see the issues you are creating
+  const repo = 'BrittanyTinnin/js-ajax-fetch-lab'
+  fetch(`https://api.github.com/repos/${repo}/issues`, 
+  {
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+  })
 }
